9c74be2e2cf6b271789980c2085fb8e9d793c48a
Hyunstone <hyun104467@naver.com>
2024-09-23 01:59:55 +0900

    feat: 글로벌 타입 및 타입 유틸 함수 구현

--- src/global/type/index.ts
+++ src/global/type/index.ts
@@ -1,3 +1,53 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+import { HttpStatus } from '@nestjs/common';
+
+export type TG<T> = T[] | T;
+export type TObj<T extends object> = { [key: string]: T };
+
+export declare namespace MemberType {
+  interface CreateRequestDto {
+    name: string;
+    email: string;
+    loginId: string;
+    password: string;
+  }
+
+  interface MemberDto {
+    id: string;
+    name: string;
+    email: string;
+  }
+
+  interface LoginRequestDto {
+    loginId: string;
+    password: string;
+  }
+
+  export interface ProficiencyDto {
+    score: number;
+    dailyResult?: object[];
+  }
+}
+
+export interface BaseResponse<T> {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  data?: T;
+}
+
+export interface ErrorCode {
+  httpStatus: HttpStatus;
+  code: string;
+  message: string;
+}
+
+export interface EnumType {
+  key: string;
+  value: string;
+}
+
 export interface ListType {
   data: any[];
   count: number;
